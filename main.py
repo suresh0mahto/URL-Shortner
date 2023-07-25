13e43fc553c89bc1cd1df398189389c8f662a110
Suresh Mahto <88234265+suresh0mahto@users.noreply.github.com>
2023-07-25 11:53:40 +0530

    main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from tkinter import *
 
 root = Tk()
 root.geometry("700x350")
-root.title("URL Shortner Application")
-root.configure(bg="#000000")
+root.title("URL Shortner")
+root.configure(bg="#2AA8F2")
 
 urlmain = StringVar()
 urlshortmain = StringVar()
@@ -19,11 +19,10 @@ def copyurl():
     urlshort = urlshortmain.get()
     pyperclip.copy(urlshort)
 
-
-Label(root,text="URL Shortner App",font=('Arial', 12)).pack(pady=10)
+Label(root,text="URL Shortner",font=('Arial', 20)).pack(pady=20)
 Entry(root,textvariable=urlmain,width=50,font=('Arial 14')).pack(padx=10, pady=10)
-Button(root,text="Generate Short Url",command=urlShortner).pack(pady=7)
+Button(root,text="Shorten URL",command=urlShortner).pack(pady=7)
 Entry(root,textvariable=urlshortmain,width=50,font=('Arial 14')).pack(padx=10, pady=10)
-Button(root,text="Copy Short Url",command=copyurl).pack(pady=5)
+Button(root,text="Copy Url",command=copyurl).pack(pady=5)
 
 root.mainloop()
