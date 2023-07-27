2e793f0c6bd6ecbef7bbc8be413d20f9c83acd3a
Suresh Mahto <88234265+suresh0mahto@users.noreply.github.com>
2023-07-27 12:04:37 +0530

    URLShortner.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import pyperclip
 import pyshorteners
 from tkinter import *
@@ -10,19 +9,23 @@ root.configure(bg="#2AA8F2")
 
 urlmain = StringVar()
 urlshortmain = StringVar()
+
+
 def urlShortner():
     urladdress = urlmain.get()
     urlshort = pyshorteners.Shortener().tinyurl.short(urladdress)
     urlshortmain.set(urlshort)
 
+
 def copyurl():
     urlshort = urlshortmain.get()
     pyperclip.copy(urlshort)
 
-Label(root,text="URL Shortner",font=('Arial', 20)).pack(pady=20)
-Entry(root,textvariable=urlmain,width=50,font=('Arial 14')).pack(padx=10, pady=10)
-Button(root,text="Shorten URL",command=urlShortner).pack(pady=7)
-Entry(root,textvariable=urlshortmain,width=50,font=('Arial 14')).pack(padx=10, pady=10)
-Button(root,text="Copy Url",command=copyurl).pack(pady=5)
+
+Label(root, text="URL Shortner", font=('Arial', 20)).pack(pady=20)
+Entry(root, textvariable=urlmain, width=50, font='Arial 14').pack(padx=10, pady=10)
+Button(root, text="Shorten URL", command=urlShortner).pack(pady=7)
+Entry(root, textvariable=urlshortmain, width=50, font='Arial 14').pack(padx=10, pady=10)
+Button(root, text="Copy Url", command=copyurl).pack(pady=5)
 
 root.mainloop()
